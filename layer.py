6b57b45b2dc97f03bef0c370fb0111c1126d4c68
akswnd98 <akswnd98@gmail.com>
2023-10-18 01:15:22 +0900

    first

---
+++ layer.py
@@ -0,0 +1,108 @@
+import numpy as np
+
+class Layer:
+  def __init__ (self):
+    pass
+
+  def propagate (self):
+    pass
+
+  def backpropagate (self, diff, lr):
+    pass
+
+class DotLayer (Layer):
+  def __init__ (self, x_generator, w_generator):
+    super().__init__()
+    self.x_generator = x_generator
+    self.w_generator = w_generator
+
+  def propagate (self):
+    self.x_generator.propagate()
+    self.w_generator.propagate()
+    self.forward_res = self.x_generator.forward_res @ self.w_generator.forward_res
+
+  def backpropagate (self, diff, lr):
+    self.w_generator.backpropagate(np.expand_dims(self.x_generator.forward_res, axis=1) @ np.expand_dims(diff, axis=0), lr)
+    self.x_generator.backpropagate(diff @ np.transpose(self.w_generator.forward_res), lr)
+
+class AddLayer (Layer):
+  def __init__ (self, x_generator, y_generator):
+    super().__init__()
+    self.x_generator = x_generator
+    self.y_generator = y_generator
+  
+  def propagate (self):
+    self.x_generator.propagate()
+    self.y_generator.propagate()
+    self.forward_res = self.x_generator.forward_res + self.y_generator.forward_res
+  
+  def backpropagate (self, diff, lr):
+    self.x_generator.backpropagate(diff, lr)
+    self.y_generator.backpropagate(diff, lr)
+
+def generate_affine_generator (x_generator, w_generator, b_generator):
+  dot_layer = DotLayer(x_generator, w_generator)
+  add_layer = AddLayer(dot_layer, b_generator)
+  return add_layer
+
+class SigmoidLayer (Layer):
+  def __init__ (self, x_generator):
+    super().__init__()
+    self.x_generator = x_generator
+
+  def propagate (self):
+    self.x_generator.propagate()
+    self.forward_res = 1 / (1 + np.exp(-self.x_generator.forward_res))
+  
+  def backpropagate(self, diff, lr):
+    self.x_generator.backpropagate(diff * self.forward_res * (1 - self.forward_res), lr)
+
+class WeightLayer (Layer):
+  def __init__ (self, w):
+    super().__init__()
+    self.w = w
+  
+  def propagate (self):
+    self.forward_res = self.w
+  
+  def backpropagate(self, diff, lr):
+    self.w -= lr * diff
+
+class BiasLayer (Layer):
+  def __init__ (self, b):
+    super().__init__()
+    self.b = b
+  
+  def propagate (self):
+    self.forward_res = self.b
+  
+  def backpropagate(self, diff, lr):
+    self.b -= lr * diff
+
+class InputLayer (Layer):
+  def __init__ (self, x):
+    super().__init__()
+    self.x = x
+
+  def propagate (self):
+    self.forward_res = self.x
+  
+  def backpropagate(self, diff, lr):
+    pass
+
+class MSELayer (Layer):
+  def __init__ (self, x_generator, label):
+    super().__init__()
+    self.x_generator = x_generator
+    self.label = label
+
+  def propagate (self):
+    self.x_generator.propagate()
+    self.forward_res = 1 / 2 * np.sum(((self.x_generator.forward_res - self.label) ** 2))
+
+  def backpropagate (self, diff, lr):
+    self.x_generator.backpropagate((self.x_generator.forward_res - self.label) * diff, lr)
+
+def generate_mse_generator (x_generator, label):
+  return MSELayer(x_generator, label)
+
---
+++ main.py
@@ -0,0 +1,42 @@
+from layer import *
+
+weights_sd = 0.01
+weights = [
+  np.random.randn(2, 32) * np.sqrt(2. / 2),
+  np.random.randn(32, 32) * np.sqrt(2. / 32),
+  np.random.randn(32, 32) * np.sqrt(2. / 32),
+  np.random.randn(32, 1) * np.sqrt(2. / 1),
+]
+
+biases = [
+  np.zeros((32, )),
+  np.zeros((32, )),
+  np.zeros((32, )),
+  np.zeros((1, )),
+]
+
+weight_layers = [WeightLayer(weight) for weight in weights]
+bias_layers = [BiasLayer(bias) for bias in biases]
+
+dataset = [[0, 0, 0], [0, 1, 1], [1, 0, 1], [1, 1, 0]]
+
+for i in range(1000000):
+  for data in dataset:
+    x_layer = InputLayer(np.array(data[0: 2]))
+    for weight_layer, bias_layer in zip(weight_layers, bias_layers):
+      x_layer = generate_affine_generator(x_layer, weight_layer, bias_layer)
+      x_layer = SigmoidLayer(x_layer)
+    mse_layer = MSELayer(x_layer, data[2])
+    mse_layer.propagate()
+    mse_layer.backpropagate(1, 0.001)
+  if i % 1000 == 0:
+    # pass
+    print(mse_layer.forward_res)
+
+for data in dataset:
+  x_layer = InputLayer(np.array(data[0: 2]))
+  for weight_layer, bias_layer in zip(weight_layers, bias_layers):
+    x_layer = generate_affine_generator(x_layer, weight_layer, bias_layer)
+    x_layer = SigmoidLayer(x_layer)
+  x_layer.propagate()
+  print(x_layer.forward_res)
